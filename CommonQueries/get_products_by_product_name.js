0c6fcdce321d78a8e51b1b75c6af06b1bfaa7fae
Mark <concamuflage@gmail.com>
2025-04-12 15:44:45 -0400

    dereferenced brand foreign key in the query.

--- CommonQueries/get_products_by_product_name.js
+++ CommonQueries/get_products_by_product_name.js
@@ -2,10 +2,47 @@ const connectDB = require("../database_connect");
 
 async function getProductsByName(nameQuery) {
   const db = await connectDB();
-  const products = await db.collection('products').find({
-    // match the string in any place, case insensitive.
-    name: { $regex: nameQuery, $options: "i" } 
-  }).toArray();
+  const products = await db.collection('products').aggregate([
+    {
+      $match: {
+        name: { $regex: nameQuery, $options: "i" }
+      }
+    },
+
+    /* 
+    the following lookup stage add a new attribute brandInfo like the following one.
+    {
+          name: "iPhone 15",
+          brand: ObjectId("..."),
+          brandInfo: [
+            {
+              _id: ObjectId("..."),
+              name: "Apple"
+            }
+          ]
+        } */
+    {
+      $lookup: {
+        from: "brands",
+        localField: "brand",  // from the perspective the products collection.
+        foreignField: "_id", //Find documents in brands where _id === product.brand
+        as: "brandInfo" // the resulting document will be stored in this field in the product doc
+      }
+    },
+    {
+      $unwind: "$brandInfo"
+    },
+    {
+      $addFields: {
+        brand: "$brandInfo.name"
+      }
+    },
+    {
+      $project: {
+        brandInfo: 0
+      }
+    }
+  ]).toArray();
   return products;
 }
 
