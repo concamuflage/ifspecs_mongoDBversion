2f0799a5200fade38f610ac89197a4a483d6050c
Mark <concamuflage@gmail.com>
2025-04-27 11:48:38 -0400

    add kill children processes

--- Sychronization/start_all.js
+++ Sychronization/start_all.js
@@ -1,21 +1,30 @@
-
 const { spawn } = require('child_process');
 
-// Define the scripts to run
 const scripts = [
   'Sychronization/atlas_to_branches.js',
   'Sychronization/from_aws_to_atlas.js',
   'Sychronization/from_gcloud_to_atlas.js'
 ];
 
-// Function to run a script
-function runScript(script) {
-  const process = spawn('node', [script], { stdio: 'inherit' });
+const children = [];
 
-  process.on('close', (code) => {
+function runScript(script) {
+  const child = spawn('node', [script], { stdio: 'inherit' });
+  children.push(child);
+
+  child.on('close', (code) => {
     console.log(`${script} exited with code ${code}`);
   });
 }
+
+// Catch SIGINT (Ctrl+C)
+process.on('SIGINT', () => {
+  console.log('Caught interrupt signal (Ctrl+C), killing child processes...');
+  children.forEach(child => {
+    child.kill('SIGINT');
+  });
+  process.exit(); // Finally exit this script
+});
 
 // Start all scripts
 scripts.forEach(runScript);
